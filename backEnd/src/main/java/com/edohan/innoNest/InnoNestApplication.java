278b328186f8c45b9880b20422ae949445c270fb
edohan <edohan0117@gmail.com>
2024-08-13 13:13:13 +0900

    BACKEND

---
+++ backEnd/src/main/java/com/edohan/innoNest/InnoNestApplication.java
@@ -0,0 +1,13 @@
+package com.edohan.innoNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoNestApplication.class, args);
+	}
+
+}
